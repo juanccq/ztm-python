8c5f256d63f27408ff218029511a13aafaa1344a
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2020-08-19 19:50:41 -0400

    adde debug query

--- file_io/file_with.py
+++ file_io/file_with.py
@@ -1,20 +1,20 @@
 # this method doesnt require to close the file
 with open('test.txt') as my_file:
-	print( my_file.readlines() )
+	print( my_file.readline() )
 
 
-with open( 'test.txt', 'r+' ) as my_file:
-	text = my_file.write( 'Hey its me' )  # carefull cause this line overwrite from the beginning of the file
+#with open( 'test.txt', 'r+' ) as my_file:
+#	text = my_file.write( 'Hey its me' )  # carefull cause this line overwrite from the beginning of the file
 
 
-with open( 'test.txt', 'r+' ) as my_file:
-	text = my_file.write( ':)' )	# carefull cause this line overwrite from the beginning of the file
+#with open( 'test.txt', 'r+' ) as my_file:
+#	text = my_file.write( ':)' )	# carefull cause this line overwrite from the beginning of the file
 
-with open( 'test.txt', 'w' ) as my_file:
-	text = my_file.write( 'all content was replaced' )	# carefull cause this line overwrite all the content
+#with open( 'test.txt', 'w' ) as my_file:
+#	text = my_file.write( 'all content was replaced' )	# carefull cause this line overwrite all the content
 
-with open( 'test.txt', 'a' ) as my_file:
-	text = my_file.write( ' added to the end of file' )
+#with open( 'test.txt', 'a' ) as my_file:
+#	text = my_file.write( ' added to the end of file' )
 
 
 # about modes
